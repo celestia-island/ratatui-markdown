cdfd0aabaafeef13d947762232438607fb026ce0
langyo <langyo.china@gmail.com>
2026-05-16 07:44:18 +0800

    feat(highlight): add 24 tree-sitter language grammars
    New languages (24 total additions):
      bash, c, cpp, c-sharp, dart, diff, elixir, haskell,
      javascript, lua, nix, objc, ocaml, php, powershell,
      r, regex, ruby, scala, solidity, swift, typescript+tsx,
      xml, yaml, zig
    
    Each is a separate Cargo feature (highlight-lang-*), all included
    in highlight-lang-all. Handles API differences between crates
    (HIGHLIGHTS_QUERY vs HIGHLIGHT_QUERY, LANGUAGE vs LANGUAGE_PHP, etc).
    
    Skipped: kotlin (no highlights query in compatible version),
    cmake/proto (no highlights query available).

--- src/highlight/treesitter.rs
+++ src/highlight/treesitter.rs
@@ -50,26 +50,137 @@ macro_rules! lang_entry {
     }};
 }
 
+macro_rules! lang_entry_sq {
+    ($lang_crate:ident) => {{
+        LangEntry {
+            language: $lang_crate::LANGUAGE.into(),
+            highlights_query: $lang_crate::HIGHLIGHT_QUERY,
+        }
+    }};
+}
+
 fn get_lang(lang: &str) -> Option<LangEntry> {
     match lang {
         #[cfg(feature = "highlight-lang-rust")]
         "rust" => Some(lang_entry!(tree_sitter_rust)),
+
         #[cfg(feature = "highlight-lang-python")]
         "python" | "py" => Some(lang_entry!(tree_sitter_python)),
+
         #[cfg(feature = "highlight-lang-go")]
         "go" | "golang" => Some(lang_entry!(tree_sitter_go)),
+
         #[cfg(feature = "highlight-lang-java")]
         "java" => Some(lang_entry!(tree_sitter_java)),
+
+        #[cfg(feature = "highlight-lang-javascript")]
+        "javascript" | "js" => Some(lang_entry_sq!(tree_sitter_javascript)),
+
+        #[cfg(feature = "highlight-lang-typescript")]
+        "typescript" | "ts" => Some(LangEntry {
+            language: tree_sitter_typescript::LANGUAGE_TYPESCRIPT.into(),
+            highlights_query: tree_sitter_typescript::HIGHLIGHTS_QUERY,
+        }),
+
+        #[cfg(feature = "highlight-lang-typescript")]
+        "tsx" => Some(LangEntry {
+            language: tree_sitter_typescript::LANGUAGE_TSX.into(),
+            highlights_query: tree_sitter_typescript::HIGHLIGHTS_QUERY,
+        }),
+
+        #[cfg(feature = "highlight-lang-c")]
+        "c" => Some(lang_entry_sq!(tree_sitter_c)),
+
+        #[cfg(feature = "highlight-lang-cpp")]
+        "cpp" | "c++" | "cxx" => Some(lang_entry_sq!(tree_sitter_cpp)),
+
+        #[cfg(feature = "highlight-lang-c-sharp")]
+        "csharp" | "c#" | "cs" => Some(lang_entry!(tree_sitter_c_sharp)),
+
+        #[cfg(feature = "highlight-lang-bash")]
+        "bash" | "sh" | "shell" | "zsh" => Some(lang_entry_sq!(tree_sitter_bash)),
+
+        #[cfg(feature = "highlight-lang-ruby")]
+        "ruby" | "rb" => Some(lang_entry!(tree_sitter_ruby)),
+
+        #[cfg(feature = "highlight-lang-swift")]
+        "swift" => Some(lang_entry!(tree_sitter_swift)),
+
+        #[cfg(feature = "highlight-lang-php")]
+        "php" => Some(LangEntry {
+            language: tree_sitter_php::LANGUAGE_PHP.into(),
+            highlights_query: tree_sitter_php::HIGHLIGHTS_QUERY,
+        }),
+
+        #[cfg(feature = "highlight-lang-scala")]
+        "scala" => Some(lang_entry!(tree_sitter_scala)),
+
+        #[cfg(feature = "highlight-lang-lua")]
+        "lua" => Some(lang_entry!(tree_sitter_lua)),
+
+        #[cfg(feature = "highlight-lang-haskell")]
+        "haskell" | "hs" => Some(lang_entry!(tree_sitter_haskell)),
+
+        #[cfg(feature = "highlight-lang-elixir")]
+        "elixir" | "ex" => Some(lang_entry!(tree_sitter_elixir)),
+
+        #[cfg(feature = "highlight-lang-yaml")]
+        "yaml" | "yml" => Some(lang_entry!(tree_sitter_yaml)),
+
+        #[cfg(feature = "highlight-lang-dart")]
+        "dart" => Some(lang_entry!(tree_sitter_dart)),
+
+        #[cfg(feature = "highlight-lang-zig")]
+        "zig" => Some(lang_entry!(tree_sitter_zig)),
+
+        #[cfg(feature = "highlight-lang-r")]
+        "r" => Some(lang_entry!(tree_sitter_r)),
+
+        #[cfg(feature = "highlight-lang-ocaml")]
+        "ocaml" => Some(LangEntry {
+            language: tree_sitter_ocaml::LANGUAGE_OCAML.into(),
+            highlights_query: tree_sitter_ocaml::HIGHLIGHTS_QUERY,
+        }),
+
+        #[cfg(feature = "highlight-lang-nix")]
+        "nix" => Some(lang_entry!(tree_sitter_nix)),
+
         #[cfg(feature = "highlight-lang-html")]
         "html" | "htm" => Some(lang_entry!(tree_sitter_html)),
+
         #[cfg(feature = "highlight-lang-css")]
         "css" | "scss" | "less" => Some(lang_entry!(tree_sitter_css)),
+
+        #[cfg(feature = "highlight-lang-xml")]
+        "xml" | "svg" | "xsd" => Some(LangEntry {
+            language: tree_sitter_xml::LANGUAGE_XML.into(),
+            highlights_query: tree_sitter_xml::XML_HIGHLIGHT_QUERY,
+        }),
+
         #[cfg(feature = "highlight-lang-json")]
         "json" => Some(lang_entry!(tree_sitter_json)),
+
         #[cfg(feature = "highlight-lang-toml")]
         "toml" => Some(lang_entry!(tree_sitter_toml_ng)),
+
         #[cfg(feature = "highlight-lang-sql")]
         "sql" => Some(lang_entry!(tree_sitter_sequel)),
+
+        #[cfg(feature = "highlight-lang-solidity")]
+        "solidity" | "sol" => Some(lang_entry_sq!(tree_sitter_solidity)),
+
+        #[cfg(feature = "highlight-lang-diff")]
+        "diff" | "patch" => Some(lang_entry!(tree_sitter_diff)),
+
+        #[cfg(feature = "highlight-lang-regex")]
+        "regex" | "regexp" => Some(lang_entry!(tree_sitter_regex)),
+
+        #[cfg(feature = "highlight-lang-powershell")]
+        "powershell" | "ps1" | "pwsh" => Some(lang_entry!(tree_sitter_powershell)),
+
+        #[cfg(feature = "highlight-lang-objc")]
+        "objc" | "objective-c" | "objectivec" => Some(lang_entry!(tree_sitter_objc)),
+
         _ => None,
     }
 }
