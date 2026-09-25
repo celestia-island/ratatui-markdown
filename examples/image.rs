b57e8e8ac893aaf89f8e78351d13589abdfed131
ruthis <66223622+ishrut@users.noreply.github.com>
2026-09-25 19:36:20 +0100

    ⬆️ bump ratatui version 0.3.

--- examples/image.rs
+++ examples/image.rs
@@ -416,7 +416,7 @@ fn main() -> anyhow::Result<()> {
                     let rect_for_proto = Rect::new(0, 0, clip.vis_w, clip.vis_h);
                     match state
                         .picker
-                        .new_protocol(img_for_proto, rect_for_proto, Resize::Fit(None))
+                        .new_protocol(img_for_proto, rect_for_proto.into(), Resize::Fit(None))
                     {
                         Ok(proto) => si.protocol = Some(proto),
                         Err(_) => {
